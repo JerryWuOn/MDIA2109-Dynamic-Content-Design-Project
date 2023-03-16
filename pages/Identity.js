9e7e3ceaee04e4d84b6a061a6e618d219460bf9a
JerryWuOn <Jon4@my.bcit.ca>
2023-03-16 14:42:55 -0700

    add pages

---
+++ pages/Identity.js
@@ -0,0 +1,9 @@
+import styles from '../styles/Identity.module.css'
+
+export default function Identity() {
+    return (
+    <>
+    <title>Identity</title>
+    </>
+    )
+}
--- pages/landingPage.js
+++
@@ -1,9 +0,0 @@
-import styles from '../styles/Warning.module.css'
-
-
-
-export default function landing() {
-    return (
-    <>landing Page</>
-    )
-}
