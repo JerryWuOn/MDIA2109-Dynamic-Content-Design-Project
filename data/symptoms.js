57a35ef0a23a74f952bbf72ae4c6d24873b62b29
JerryWuOn <Jon4@my.bcit.ca>
2023-03-19 21:39:10 -0700

    add symptoms to page

---
+++ data/symptoms.js
@@ -0,0 +1,32 @@
+<>
+    /** Stomach */
+    <>
+    Stomach Ache, 
+    Upset Stomach, 
+    Acid Reflux
+    </>
+    /** Eyes */
+    <>
+    Dry eyes, 
+    Red eyes, 
+    Blurry Vision
+    </>
+    /** Head */
+    <>
+    Nasuea, 
+    Spinning Sensation, 
+    Lightheadness
+    </>
+    /** Nose */
+    <>
+    Sneezing, 
+    Congestion, 
+    Cough
+    </>
+    /** Muscles */
+    <>
+    Pain or Tenderness, 
+    Muscle Weakness, 
+    Limited Motion
+    </>
+</>
