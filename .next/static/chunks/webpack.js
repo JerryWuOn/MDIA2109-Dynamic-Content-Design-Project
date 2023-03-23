1c07b79281e668f1e42bc39f5a5d7d34180195ca
JerryWuOn <Jon4@my.bcit.ca>
2023-03-23 09:19:50 -0700

    delete default template

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "2207f6e294c3e7a5"; }
+/******/ 		__webpack_require__.h = function() { return "011e85d2afacb270"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
