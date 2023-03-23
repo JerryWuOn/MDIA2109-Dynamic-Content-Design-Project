d34600a728acbe166583f41d96afc832adf41d73
JerryWuOn <Jon4@my.bcit.ca>
2023-03-23 09:24:43 -0700

    fixing issue

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "011e85d2afacb270"; }
+/******/ 		__webpack_require__.h = function() { return "16a9ec793a6fb8d6"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
