1021fdba492026c262cc231dd82052baf5014158
RonanKhalilOlaes <113484040+RonanKhalilOlaes@users.noreply.github.com>
2023-04-14 12:28:35 -0700

    Reference page_updated

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "26f53604e6b0c660"; }
+/******/ 		__webpack_require__.h = function() { return "34ef62dddc05ea75"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- pages/References.js
+++ pages/References.js
@@ -1,8 +1,10 @@
+import Head from 'next/head'
 import styles from '../styles/References.module.css'
 import Topbar from '../components/topbar/index'
 import Link from 'next/link'
 import { useState, useEffect } from 'react'
 import Navbar from '../components/navbar'
+
 
 export default function References() {
     return (
@@ -12,6 +14,24 @@ export default function References() {
         <Topbar/>
         <h1>References</h1>
         <p>Quick links to the primary medication for each category:</p>
+
+        <div className={styles.cards}>
+            <h2>Eye medication</h2>
+            <p>Advil</p>
+            <a className={styles.link}>Learn more</a>
+        </div>
+        
+        <div className={styles.cards}>
+            <h2>Head medication</h2>
+            <p>Advil</p>
+            <a className={styles.link}>Learn more</a>
+        </div>
+        
+        <div className={styles.cards}>
+            <h2>Nose medication</h2>
+            <p>Advil</p>
+            <a className={styles.link}>Learn more</a>
+        </div>
         
         <Navbar className={styles.nav}/>
         
