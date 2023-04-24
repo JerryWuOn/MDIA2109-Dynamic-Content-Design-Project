fd926ca59cda5f1affb678552eeec78267f3e65f
JerryWuOn <Jon4@my.bcit.ca>
2023-04-24 10:08:37 -0700

    update gitignore

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "98a38e77a0242c17"; }
+/******/ 		__webpack_require__.h = function() { return "f12a9a04ffbc1317"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
