f2cf63216f09605d43322554a22c234dd23c1cca
TakamiInagaki <takami.l.inagaki@gmail.com>
2023-04-24 10:27:48 -0700

    bug

--- .next/static/development/_buildManifest.js
+++ .next/static/development/_buildManifest.js
@@ -1,5 +1,1 @@
-<<<<<<< HEAD
-self.__BUILD_MANIFEST = {__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static\u002Fchunks\u002Fpages\u002Findex.js"],"/Category":["static\u002Fchunks\u002Fpages\u002FCategory.js"],"/EnterYou":["static\u002Fchunks\u002Fpages\u002FEnterYou.js"],"/Identify":["static\u002Fchunks\u002Fpages\u002FIdentify.js"],"/Startpage":["static\u002Fchunks\u002Fpages\u002FStartpage.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error.js"],sortedPages:["\u002F","\u002FCategory","\u002FEnterYou","\u002FIdentify","\u002FStartpage","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
-=======
-self.__BUILD_MANIFEST = {__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/Category":["static\u002Fchunks\u002Fpages\u002FCategory.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error.js"],sortedPages:["\u002FCategory","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
->>>>>>> c9247ae1732a0a36a9a6cbfdcadf1e029c41fd63
+self.__BUILD_MANIFEST = {__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/_error":["static\u002Fchunks\u002Fpages\u002F_error.js"],sortedPages:["\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
