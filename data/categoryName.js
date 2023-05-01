2a99d11e200224da94a8e42454b40129e586bdf8
JerryWuOn <Jon4@my.bcit.ca>
2023-05-01 12:00:12 -0700

    update data

---
+++ data/categoryName.js
@@ -0,0 +1,54 @@
+import { useState } from "react";
+
+
+export const ParentInfo = {
+
+    symptoms: [
+        {
+            category: "Head",
+            description: "Down below are common symptoms for head injuries:",
+            symptomsone: "Nausea",
+            symptomstwo: "Spinning Sensation",
+            symptomsthree: "Lightheadedness",
+            symptomsfour: "None of the above",  
+        }, 
+        {
+            category: "Nose",
+            description: "Down below are common symptoms for head injuries:",
+            symptomsone: "Nasal Congestion",
+            symptomstwo: "Runny Nose",
+            symptomsthree: "Blockage",
+            symptomsfour: "None of the above",
+        }, 
+        {
+            category: "Stomach",
+            description: "Down below are common symptoms for head injuries:",
+            symptomsone: "Stomach Ache",
+            symptomstwo: "Upset Stomach",
+            symptomsthree: "Acid Reflux",
+            symptomsfour: "None of the above",
+        }, 
+        {
+            category: "Eyes",
+            description: "Down below are common symptoms for head injuries:",
+            symptomsone: "Dry Eyes",
+            symptomstwo:  "Red Eyes",
+            symptomsthree:  "Blurry Vision",
+            symptomsfour: "None of the above",
+        }, 
+        {
+            category: "Muscles",
+            description: "Down below are common symptoms for head injuries:",
+            symptomsone: "Pain or tenderness",
+            symptomstwo: "Soreness",
+            symptomsthree: "Limited Motion",
+            symptomsfour: "None of the above",
+        },
+        {
+            category: "Other",
+            description: "Down below write the symptom you are looking for",
+            symptomsone: "Nausea",
+            symptomstwo: "Spinning Sensation",
+            symptomsthree: "Lightheadedness",
+            symptomsfour: "None of the above",
+        }
