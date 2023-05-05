5a3c0e62de22b9b6d85d2a2b19e909a34df48b4d
TakamiInagaki <takami.l.inagaki@gmail.com>
2023-05-04 19:49:01 -0700

    add quiz page

--- pages/Quiz.js
+++ pages/Quiz.js
@@ -1,6 +1,8 @@
 import styles from '../styles/Quiz.module.css'
 import Head from 'next/head'
 import Topbar from '../components/topbar'
+import Navbar from '../components/navbar'
+import Link from 'next/link'
 
 export default function Quiz(){
 
@@ -21,7 +23,17 @@ export default function Quiz(){
         </div>
         
 
+        <h1>Lets check your knowdklege! </h1>
+
+        <Link href="">
+        <button></button>
+        </Link>
+
             </main>
+
+            <div className={styles.navbar}>
+                <Navbar/>
+            </div>
         </div>
         </>
     )
