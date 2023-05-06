a071921ab5a8e304060e33fa30ecfe912b39c9f8
RonanKhalilOlaes <113484040+RonanKhalilOlaes@users.noreply.github.com>
2023-05-06 15:57:13 -0700

    update

--- .next/static/development/_buildManifest.js
+++ .next/static/development/_buildManifest.js
@@ -1,5 +1,1 @@
-<<<<<<< HEAD
-self.__BUILD_MANIFEST = {__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/_error":["static\u002Fchunks\u002Fpages\u002F_error.js"],sortedPages:["\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
-=======
-self.__BUILD_MANIFEST = {__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/Quiz":["static\u002Fchunks\u002Fpages\u002FQuiz.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error.js"],sortedPages:["\u002FQuiz","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
->>>>>>> b8b35c6ea1142be707bd9164af9374d43bad6435
+self.__BUILD_MANIFEST = {__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/ResultsHead":["static\u002Fchunks\u002Fpages\u002FResultsHead.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error.js"],"/symptomsHead":["static\u002Fchunks\u002Fpages\u002FsymptomsHead.js"],sortedPages:["\u002FResultsHead","\u002F_app","\u002F_error","\u002FsymptomsHead"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
